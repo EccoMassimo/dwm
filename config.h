4288236119b5768bb1c375d187a5fed2b1f91641
EccoMassimo <massimo@cdrateline.com>
2019-12-13 18:43:31 -0500

    modified:   config.h 	modified:   dwm 	modified:   dwm.c 	modified:   dwm.o

--- config.h
+++ config.h
@@ -40,7 +40,6 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
 };
 
 /* key definitions */
@@ -90,7 +89,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_q,                  killclient,     {0} },
 	{ MODKEY,                       XK_t,                  setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,                  setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,                  setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,              setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,              togglefloating, {0} },
 	{ MODKEY,                       XK_comma,              focusmon,       {.i = -1 } },
