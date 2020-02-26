2ff7b23ca352030b5cc7604662ee2e3b7895743c
Max Amundsen <massimo@cdrateline.com>
2020-02-26 17:18:58 -0500

    modified:   config.h 	modified:   drw.o 	modified:   dwm 	modified:   dwm.o 	modified:   util.o

--- config.h
+++ config.h
@@ -1,14 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;       	/* border pixel of windows */
+static const unsigned int borderpx  = 1;       	/* border pixel of windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const char *fonts[]          = { "DejaVu Sans Mono:size=10" };
 static const char dmenufont[]       = "DejaVu Sans Mono:size=10";
@@ -32,8 +32,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Alsamixer",     NULL,       NULL,       0,            1,           0 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "null",     NULL,       NULL,       0,            1,           0 },
+	{ "null",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -48,7 +48,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -61,37 +61,26 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-b", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
+static const char *volumeup[] = { "amixer", "set", "Master", "10%+", NULL};
+static const char *volumedown[] = { "amixer", "set", "Master", "10%-", NULL};
 
 /* Program Shortcuts */
-static const char *browsercmd[] = { "chromium", NULL };
-static const char *filecmd[] = { "pcmanfm", NULL };
+static const char *browsercmd[] = { "chromium-browser-privacy", NULL };
+static const char *filecmd[] = { "nautilus", NULL };
 static const char *guitext[] = { "codium", NULL };
-static const char *msg[] = { "claws-mail", NULL };
-static const char *calcurse[] = { "st", "-e", "calcurse", NULL };
-static const char *ranger[] = { "st", "-e", "ranger", NULL };
-static const char *vim [] = { "st", "-e", "vim", NULL };
-static const char *irssi[] = { "st", "-e", "irssi", NULL };
+static const char *msg[] = { "geary", NULL };
 static const char *scrot[] = { "xfce4-screenshooter", NULL };
-static const char *alsa[] = { "st", "-e", "alsamixer", NULL };
-static const char *htop[] = { "st", "-e", "htop", NULL };
-
-static const char *i3lock[] = {"i3lock", "-c", "000000", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,             XK_l,                  spawn,          {.v = i3lock } },
+	{ MODKEY,                       XK_F6,                 spawn,          {.v = volumeup} },
+	{ MODKEY,                       XK_F5,                 spawn,          {.v = volumedown} },
 	{ MODKEY,                       XK_F1,                 spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_F2,                 spawn,          {.v = filecmd } },
 	{ MODKEY,                       XK_F3,                 spawn,          {.v = guitext } },
 	{ MODKEY,                       XK_F4,                 spawn,          {.v = msg } },
-	{ MODKEY,                       XK_F5,                 spawn,          {.v = calcurse } },
-	{ MODKEY,                       XK_F6,                 spawn,          {.v = ranger } },
-	{ MODKEY,                       XK_F7,                 spawn,          {.v = vim } },
-	{ MODKEY,                       XK_F8,                 spawn,          {.v = irssi } },
 	{ MODKEY,                       XK_Print,              spawn,          {.v = scrot } },
-	{ MODKEY, 			XK_Scroll_Lock,        spawn,          {.v = alsa } },
-	{ MODKEY,                       XK_Pause,              spawn,          {.v = htop } },
 	{ MODKEY,                       XK_d,                  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,             spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_Down,               focusstack,     {.i = +1 } },
